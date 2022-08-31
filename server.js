3e3d4b1d9b1eeb3de5addbcb60d6ab338429fe99
Artem Rachkov <a.rachkov94@gmail.com>
2022-08-31 08:56:04 +0300

    fix server

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 app.use(express.static(__dirname + "/dist/angular-movies-explorer"));
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+console.log(`Started at port: ${port}`);
+app.listen(port);
