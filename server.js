82c83e065c790fbd99f441d54c13bd791da12409
Artem Rachkov <a.rachkov94@gmail.com>
2022-08-31 10:07:13 +0300

    fix routing on server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 app.use(express.static(__dirname + "/dist/angular-movies-explorer"));
+app.get("*", (req, res) => {
+  res
+    .status(200)
+    .sendFile(
+      path.join(__dirname, "/dist/angular-movies-explorer", "index.html")
+    );
+});
 const port = process.env.PORT || 3000;
 console.log(`Started at port: ${port}`);
 app.listen(port);
