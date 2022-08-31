4bafdc455d5b1121e4029954e4a6c6eb3b16e2af
Artem Rachkov <a.rachkov94@gmail.com>
2022-08-31 09:12:01 +0300

    add replace to dev mode

--- replace.env.ts
+++ replace.env.ts
@@ -1,10 +1,14 @@
 const replace = require('replace-in-file');
 require('dotenv').config();
 
-const { API_URL, KINOPOISK_API_URL, KINOPOISK_API_KEY } = process.env;
+const { API_URL, KINOPOISK_API_URL, KINOPOISK_API_KEY, NODE_ENV } = process.env;
+
+const isProd = NODE_ENV === 'production';
 
 const options = {
-  files: './src/environments/environment.prod.ts',
+  files: isProd
+    ? './src/environments/environment.prod.ts'
+    : './src/environments/environment.ts',
   from: [/{API_URL}/g, /{KINOPOISK_API_URL}/g, /{KINOPOISK_API_KEY}/g],
   to: [API_URL, KINOPOISK_API_URL, KINOPOISK_API_KEY],
   allowEmptyPaths: false,
@@ -12,7 +16,15 @@ const options = {
 
 try {
   replace.sync(options);
-  console.log('Build version set: ' + process.env);
+  console.log(
+    'Build version set: ' +
+      JSON.stringify({
+        API_URL,
+        KINOPOISK_API_URL,
+        KINOPOISK_API_KEY,
+        NODE_ENV,
+      })
+  );
 } catch (error) {
   console.error('Error occurred:', error);
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://express-movies-explorer.herokuapp.com',
+  kinopoiskApiUrl: 'https://kinopoiskapiunofficial.tech/api',
+  kinopoiskApiKey: '7a4b6ab7-2f0b-469d-aa63-0e41a39d90a1',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
