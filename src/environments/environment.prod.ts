96754fdd6a0986ed90daff299f130e062f81c599
Artem Rachkov <a.rachkov94@gmail.com>
2022-08-31 09:43:33 +0300

    add env to gitignore and remove dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '{API_URL}',
-  kinopoiskApiUrl: '{KINOPOISK_API_URL}',
-  kinopoiskApiKey: '{KINOPOISK_API_KEY}',
+  apiUrl: 'https://express-movies-explorer.herokuapp.com',
+  kinopoiskApiUrl: 'https://kinopoiskapiunofficial.tech/api',
+  kinopoiskApiKey: '7a4b6ab7-2f0b-469d-aa63-0e41a39d90a1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://express-movies-explorer.herokuapp.com',
   kinopoiskApiUrl: 'https://kinopoiskapiunofficial.tech/api',
   kinopoiskApiKey: '7a4b6ab7-2f0b-469d-aa63-0e41a39d90a1',
